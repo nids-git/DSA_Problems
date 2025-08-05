5ed2e3ea6c89eeaf9ae08d6a7375a664d147f3df
nidhibizzmanweb <nidhi.bizzmanweb@gmail.com>
2025-08-05 13:41:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dsa_problems/arrays/slidingwindow/ReverseArray.java
@@ -0,0 +1,29 @@
+package com.example.dsa_problems.arrays.slidingwindow;
+
+public class ReverseArray {
+
+    public static void reverseArray(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        displayArray(arr);
+    }
+
+    public static void displayArray(int[] arr){
+        for(int i=0;i<arr.length; i++){
+            System.out.print(arr[i]+", ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {34,78,90,12,34};
+        reverseArray(arr);
+    }
+}
